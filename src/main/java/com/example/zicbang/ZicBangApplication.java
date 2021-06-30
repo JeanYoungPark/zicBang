747b9333a8708b15b5c25c13dea3473ae3a77fed
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-06-30 19:59:23 +0900

    Initial commit

---
+++ src/main/java/com/example/zicbang/ZicBangApplication.java
@@ -0,0 +1,13 @@
+package com.example.zicbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZicBangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZicBangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zicbang/ZicBangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zicbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZicBangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
