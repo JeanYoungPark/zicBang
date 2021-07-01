76e2642331c716816e7786914f6416c2ded098d6
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-07-01 18:51:49 +0900

    style: main 디자인 완료

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+    $('.tab li').on('click',function(){
+        $('.tab li').removeClass('on');
+        $(this).addClass('on');
+        var placeholder = $(this).find('span').attr('data-txt');
+        $('.search .input input').attr('placeholder',placeholder);
+    });
 });
